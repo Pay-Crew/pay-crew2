775bf2670eb7586551adaf8b686ef0229bdc56b1
Myxogastria0808 <r.rstudio.c@gmail.com>
2026-01-17 04:47:01 +0900

    chore: update group list css

--- products/frontend/src/routes/Root/components/Group/index.tsx
+++ products/frontend/src/routes/Root/components/Group/index.tsx
@@ -25,20 +25,23 @@ const Group: FC<Props> = (props: Props) => {
   return (
     <>
       <SubTitle subTitle="所属グループ" />
-      <ul className={styles.groupUl}>
+      <ul className={styles.ul}>
         {props.groups.map((group) => (
-          <li className={styles.groupLi} key={group.group_id}>
-            <Link className={styles.groupLink} to={`/group/${group.group_id}`}>
-              <h3 className={styles.groupName}>{group.group_name}</h3>
-              <SquareArrowOutUpRight />
-              <div className={styles.groupHeader}>
-                <div className={styles.createdByWrapper}>
-                  <small className={styles.createdByLabel}>created by&thinsp;:&nbsp;</small>
-                  <small className={styles.createdBy}>{group.created_by_name}</small>
-                </div>
+          <li className={styles.li} key={group.group_id}>
+            <Link className={styles.link} to={`/group/${group.group_id}`}>
+              {/* section 1 */}
+              <div className={styles.groupNameWrapper}>
+                <h3 className={styles.groupName}>{group.group_name}</h3>
+                <SquareArrowOutUpRight className={styles.icon} />
               </div>
-              <div className={styles.memberBox}>
-                <small className={styles.memberLabel}>[メンバー]</small>
+              {/* section 2 */}
+              <div className={styles.createdByWrapper}>
+                <small className={styles.label}>created by&thinsp;:&nbsp;</small>
+                <small className={styles.createdBy}>{group.created_by_name}</small>
+              </div>
+              {/* section 3 */}
+              <div className={styles.memberWrapper}>
+                <small className={styles.label}>[メンバー]</small>
                 <p className={styles.memberNames}>
                   {group.members.map((member, index) =>
                     index === group.members.length - 1 ? `${member.user_name}` : `${member.user_name}、`
